7c64cb7d654c4129bc92c259bee55b5de80ce782
Serhii Kurnosenko <81823080+kukuruzvelt@users.noreply.github.com>
2023-01-17 15:44:55 +0200

    Update

--- src/main/java/com/example/testtask/dao/entity/Person.java
+++ src/main/java/com/example/testtask/dao/entity/Person.java
@@ -1,16 +1,23 @@
 package com.example.testtask.dao.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "Persons")
 @ToString
+@Getter
+@Setter
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Integer id;
+    @Column
     private String name;
+    @Column
     private String surname;
+    @Column
     private String birthday;
 }
