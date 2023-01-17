269d6fd52ec0decd24c0090eb0bdb77f798fa567
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-01-17 19:44:25 +0200

    Updating data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (1, 'name1', 'surname', '2000-11-20');
-INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (2, 'name2', 'surname', '2000-11-20');
-INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (3, 'name3', 'surname', '2000-11-20');
-INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (4, 'name4', 'surname', '2000-11-20');
+INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (1, 'name1', 'surname1', '2000-11-20');
+INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (2, 'name2', 'surname2', '1999-03-14');
+INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (3, 'name3', 'surname3', '2005-09-25');
+INSERT INTO persons (`id`, `name`, `surname`, `birthday`) VALUES (4, 'name4', 'surname4', '1987-12-08');
 
 
