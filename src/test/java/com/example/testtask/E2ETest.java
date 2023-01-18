7c2ef4e12fa1d42f2476970a6f821963aa68efde
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-01-18 18:21:57 +0200

    Updating e2e test

--- src/test/java/com/example/testtask/E2ETest.java
+++ src/test/java/com/example/testtask/E2ETest.java
@@ -1,36 +1,43 @@
 package com.example.testtask;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.example.testtask.dao.repository.PersonsCrudRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = TestTaskApplication.class)
+@TestPropertySource(locations = "classpath:application.properties")
 @AutoConfigureMockMvc
 public class E2ETest {
 
   @Autowired
   private MockMvc mvc;
 
-  @Autowired
-  private PersonsCrudRepository personsCrudRepository;
-
   @Test
   public void getUserTest() throws Exception {
     mvc.perform(get("/get-person?id=1")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.name").value("name1"));
-  }
+        .andExpect(jsonPath("$.name").value("name1"))
+        .andExpect(jsonPath("$.surname").value("surname1"))
+        .andExpect(jsonPath("$.age").value("22"));
 
+    mvc.perform(get("/get-person?id=4")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.name").value("name4"))
+        .andExpect(jsonPath("$.surname").value("surname4"))
+        .andExpect(jsonPath("$.age").value("35"));
+
+    mvc.perform(get("/get-person?id=5"))
+        .andExpect(content().string("Cannot find person with this id"));
+  }
 }
