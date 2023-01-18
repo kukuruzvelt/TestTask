83e16ba8573e873681328d8968a6cc1f6b5074c4
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-01-18 15:53:14 +0200

    Updating e2e test

--- src/test/java/com/example/testtask/E2ETest.java
+++ src/test/java/com/example/testtask/E2ETest.java
@@ -5,11 +5,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.testtask.dao.repository.PersonsCrudRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -20,12 +22,15 @@ public class E2ETest {
   @Autowired
   private MockMvc mvc;
 
+  @Autowired
+  private PersonsCrudRepository personsCrudRepository;
+
   @Test
   public void getUserTest() throws Exception {
-    mvc.perform(get("/get-user?id=1")
+    mvc.perform(get("/get-person?id=1")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
-        .andExpect((ResultMatcher) jsonPath("$[0].name", is("name1")));
+        .andExpect(jsonPath("$.name").value("name1"));
   }
 
 }
